7c1ce6bbcd08a1a77f0c6f6dc337c7aa0ae13034
raul yarmemmedov <raulyarmemmedov@gmail.com>
2024-05-26 18:01:45 +0400

    updated

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from calendar_app import CalendarApp
 
 def main():
-    server_name = 'DESKTOP-VIKK52P'  
+    server_name = 'DESKTOP-NA23RM9'  
     database_name = 'Calendar'  
 
     app = CalendarApp(server_name, database_name)
